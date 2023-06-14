ee41c02574afe46db953e4246fb478e39a46a893
Tasley Martins <tasleym@gmail.com>
2023-06-14 14:32:40 -0300

    Inicialização do repositório

---
+++ src/main/java/br/com/rodarte/testejava/TesteEmJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodarte.testejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEmJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodarte/testejava/TesteEmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodarte.testejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
