b57bea42877aabdb4e715becec0c10afc43a1e6f
Tasley Martins <tasleym@gmail.com>
2023-06-15 14:36:07 -0300

    Ajuste no arquivo do React para corrigir a ordem dos metodos

--- React/teste-rodarte/src/App.js
+++ React/teste-rodarte/src/App.js
@@ -4,6 +4,14 @@ import React, { useState } from 'react';
 const App = () => {
   const [output, setOutput] = useState('');
 
+  const handleImportData = () => {
+    fetch("http://localhost:8080/notas/extrair-excel", { method: "PATCH" })
+      .then(response => response.text())
+      .then(data => {
+        setOutput(data);
+      });
+  };
+
   const handleListAll = () => {
     fetch("http://localhost:8080/notas/todos")
       .then(response => response.json())
@@ -47,20 +55,12 @@ const App = () => {
       });
   };
 
-  const handleImportData = () => {
-    fetch("http://localhost:8080/notas/extrair-excel", { method: "PATCH" })
-      .then(response => response.text())
-      .then(data => {
-        setOutput(data);
-      });
-  };
-
   const handleOrganizedTable = () => {
     fetch("http://localhost:8080/notas/tabela-organizada")
       .then(response => response.json())
       .then(data => {
         let outputHTML = `
-        <table className="styled-table centered-table">
+        <table>
           <tr>
             <th>Nome</th>
             <th>Média de Notas</th>
@@ -94,8 +94,8 @@ const App = () => {
   return (
     <div>
       <h1>Notas App</h1>
-      <button onClick={handleListAll}>Listar todos os alunos</button>
       <button onClick={handleImportData}>Importar dados do Excel</button>
+      <button onClick={handleListAll}>Listar todos os alunos</button>
       <button onClick={handleOrganizedTable}>Tabela organizada</button>
       <button onClick={handleExportExcel}>Exportar tabela organizada para o Excel</button>
       <div id="output" dangerouslySetInnerHTML={{ __html: output }}></div>
