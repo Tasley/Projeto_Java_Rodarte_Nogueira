06d21a6cb22b9c1afbd2baa60c4638f9ef39700e
Tasley Martins <tasleym@gmail.com>
2023-06-15 09:00:55 -0300

    Ajuste no Repository para não mostrar o ID

--- src/main/java/br/com/rodarte/testejava/repository/NotasRepository.java
+++ src/main/java/br/com/rodarte/testejava/repository/NotasRepository.java
@@ -8,16 +8,15 @@ import java.util.List;
 
 public interface NotasRepository extends JpaRepository<Notas, Long> {
 
-    @Query(value = "SELECT identificacao AS identificacao, nome AS nome, " +
+    @Query(value = "SELECT nome AS nome, " +
             "FORMAT((SUM(nota_trimestre_um+nota_trimestre_dois+nota_trimestre_tres)/3),2) AS media, " +
             "FLOOR(DATEDIFF(CURDATE(), data_nascimento) / 365) AS idade " +
             "FROM notas.t_notas " +
-            "GROUP BY nome, data_nascimento, identificacao " +
+            "GROUP BY nome, data_nascimento " +
             "ORDER BY idade ASC", nativeQuery = true)
     List<TabelaNotasDTO> tabelaOrganizada();
 
     interface TabelaNotasDTO {
-        String getIdentificacao();
 
         String getNome();
 
